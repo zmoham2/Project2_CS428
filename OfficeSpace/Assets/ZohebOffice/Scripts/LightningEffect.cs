d001bf0a6400d6425d734c51d3206779cbab4276
zmoham2 <zmoham2@uic.edu>
2019-10-05 03:57:17 -0500

    added some cool effects along with interactable

---
+++ OfficeSpace/Assets/ZohebOffice/Scripts/LightningEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningEffect : MonoBehaviour
+{
+    public GameObject myobject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(lightitup());
+    }
+
+    IEnumerator lightitup()
+    {
+        myobject.SetActive(false);
+
+        //Wait for 54 seconds
+        yield return new WaitForSeconds(54);
+        myobject.SetActive(true);
+        yield return new WaitForSeconds(1);
+        myobject.SetActive(false);
+        yield return new WaitForSeconds(1 / 2);
+        myobject.SetActive(true);
+        yield return new WaitForSeconds(1 / 2);
+        myobject.SetActive(false);
+        yield return new WaitForSeconds(1 / 2);
+        myobject.SetActive(true);
+        yield return new WaitForSeconds(1 / 2);
+        myobject.SetActive(false);
+
+
+    }
+}
