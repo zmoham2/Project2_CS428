4285bf5def492be47512ff705874fc1b0d5efffb
zmoham2 <zmoham2@uic.edu>
2019-10-05 16:57:10 -0500

    trying switch interactable

---
+++ OfficeSpace/Assets/onoffswitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onoffswitch : MonoBehaviour
+{
+    public GameObject myobject, object2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RunLight();
+    }
+
+    // Update is called once per frame
+    void RunLight()
+    {
+        while (true)
+        {
+            if (myobject.activeSelf == true && object2.activeSelf == true)
+            {
+                myobject.SetActive(false);
+                object2.SetActive(false);
+            }
+            else
+            {
+                myobject.SetActive(true);
+                object2.SetActive(true);
+            }
+        }
+
+    }
+    void Update()
+    {
+       
+    }
+}
