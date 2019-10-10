3bd8f03dadf0c8e822ad39d0dcb78a69087589be
MDYbarra <mybarr3@uic.edu>
2019-10-10 18:00:20 -0500

    added toggle script

---
+++ OfficeSpace/Assets/tv_control.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tv_control : MonoBehaviour
+{
+
+   public GameObject tv_toggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void tvOnOff()
+    {
+
+        if(!tv_toggle.activeInHierarchy)
+        {
+            tv_toggle.SetActive(true);
+        }
+
+
+
+        else if(tv_toggle.activeInHierarchy)
+        {
+            tv_toggle.SetActive(false);
+        }
+
+
+    }
+}
