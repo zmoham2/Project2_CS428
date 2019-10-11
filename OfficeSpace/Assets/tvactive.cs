fdb493f08a900d4938292a7f658e6df398be470e
zmoham2 <zmoham2@uic.edu>
2019-10-10 20:51:43 -0500

    fixed batTV script

---
+++ OfficeSpace/Assets/tvactive.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tvactive : MonoBehaviour
+{
+
+    public GameObject batmantas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void tvOnOff()
+    {
+
+        if (!batmantas.activeInHierarchy)
+        {
+            batmantas.SetActive(true);
+        }
+
+
+
+        else if (batmantas.activeInHierarchy)
+        {
+            batmantas.SetActive(false);
+        }
+
+
+    }
+}
