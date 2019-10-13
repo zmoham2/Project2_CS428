3e9ab390b81981ac84b31c938deed6899cf3982e
zmoham2 <zmoham2@uic.edu>
2019-10-12 22:38:22 -0500

    included scaling prototype

---
+++ OfficeSpace/Assets/localscale.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class localscale : MonoBehaviour
+{
+    public float x = 0.1f;
+    public float y = 0.1f;
+    public float z = 0.1f;
+    //public float px = 0.1f;
+    //public float py = 0.1f;
+    //public float pz = 0.1f;
+    float counter = 0;
+
+    void Update()
+    {
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (counter == 0)
+        {
+            //update the position
+            transform.localScale -= new Vector3(x, y, z);
+            // Widen the object by x, y, and z values
+            transform.position = transform.position + new Vector3(horizontalInput, verticalInput, 0);
+
+            counter++;
+        }
+    }
+}
---
+++ OfficeSpace/Assets/normalizeSize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class normalizeSize : MonoBehaviour
+{
+    
+    float counter = 0;
+
+    void Update()
+    {
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (counter == 0)
+        {
+            //update the position
+            transform.localScale = new Vector3(1, 1, 1);
+            // Widen the object by x, y, and z values
+            transform.position = transform.position + new Vector3(0, 0, 0);
+
+            counter++;
+        }
+    }
+}
