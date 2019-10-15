cf5a62a813eca35af66173587b1faa216db987d8
zmoham2 <zmoham2@uic.edu>
2019-10-15 14:24:53 -0500

    edited scaling when big/small, added joystick

--- OfficeSpace/Assets/ShrinkSubject.cs
+++ OfficeSpace/Assets/ShrinkSubject.cs
@@ -6,6 +6,7 @@ public class ShrinkSubject : MonoBehaviour
 {
     public GameObject ToSize;
     public GameObject Shrink, Enlarge, Normalize, Ceiling;
+    
     int counter = 0;
 
     // Start is called before the first frame update
@@ -18,20 +19,60 @@ public class ShrinkSubject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ScaleDown();
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            ScaleNormal();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ScaleBig();
+        }
+        if (Input.GetKey(KeyCode.JoystickButton9))
+        {
+            ScaleNormal();
+        }
+        if (Input.GetKey(KeyCode.JoystickButton17))
+        {
+            ScaleDown();
+        }
+
     }
 
     public void ScaleDown()
     {
-        //get the Input from Horizontal axis
-        float horizontalInput = Input.GetAxis("Horizontal");
-        //get the Input from Vertical axis
-        float verticalInput = Input.GetAxis("Vertical");
-
+        
+        if (Enlarge.activeSelf == false)
+        {
+            //get the Input from Horizontal axis
+            float horizontalInput = Input.GetAxis("Horizontal");
+            //get the Input from Vertical axis
+            float verticalInput = Input.GetAxis("Vertical");
             //update the position
-            ToSize.transform.localScale -= new Vector3(0.7f, .7f, 0.7f);
+            ToSize.transform.localScale -= new Vector3(4.4f, 4.4f, 4.4f);
             // Widen the object by x, y, and z values
             ToSize.transform.position = ToSize.transform.position + new Vector3(horizontalInput, verticalInput, 0);
+            Debug.Log("We came from big state");
+
+        }
+        else if (Enlarge.activeSelf == true)
+        {
+            //get the Input from Horizontal axis
+            float horizontalInput = Input.GetAxis("Horizontal");
+            //get the Input from Vertical axis
+            float verticalInput = Input.GetAxis("Vertical");
+            //update the position
+            ToSize.transform.localScale -= new Vector3(0.7f, 0.7f, 0.7f);
+            // Widen the object by x, y, and z values
+            ToSize.transform.position = ToSize.transform.position + new Vector3(horizontalInput, verticalInput, 0);
+            Debug.Log("We came from normal state");
+        }
+
         Shrink.SetActive(false);
         Normalize.SetActive(true);
         Enlarge.SetActive(true);
@@ -41,15 +82,34 @@ public class ShrinkSubject : MonoBehaviour
     }
     public void ScaleBig()
     {
-        //get the Input from Horizontal axis
-        float horizontalInput = Input.GetAxis("Horizontal");
-        //get the Input from Vertical axis
-        float verticalInput = Input.GetAxis("Vertical");
+        
 
-        //update the position
-        ToSize.transform.localScale += new Vector3(4f, 4f, 4f);
-        // Widen the object by x, y, and z values
-        ToSize.transform.position = ToSize.transform.position + new Vector3(horizontalInput, verticalInput, 0);
+        if (Shrink.activeSelf == false)
+        {
+            //get the Input from Horizontal axis
+            float horizontalInput = Input.GetAxis("Horizontal");
+            //get the Input from Vertical axis
+            float verticalInput = Input.GetAxis("Vertical");
+            //update the position
+            ToSize.transform.localScale += new Vector3(4.4f, 4.4f, 4.4f);
+            // Widen the object by x, y, and z values
+            ToSize.transform.position = ToSize.transform.position + new Vector3(horizontalInput, verticalInput, 0);
+
+        }
+        else if (Shrink.activeSelf == true)
+        {
+            //get the Input from Horizontal axis
+            float horizontalInput = Input.GetAxis("Horizontal");
+            //get the Input from Vertical axis
+            float verticalInput = Input.GetAxis("Vertical");
+            //update the position
+            ToSize.transform.localScale += new Vector3(2f, 2f, 2f);
+            // Widen the object by x, y, and z values
+            ToSize.transform.position = ToSize.transform.position + new Vector3(horizontalInput, verticalInput, 0);
+        }
+        
+
+
         Shrink.SetActive(true);
         Normalize.SetActive(true);
         Enlarge.SetActive(false);
