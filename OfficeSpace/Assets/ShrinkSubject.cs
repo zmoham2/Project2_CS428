a6b440e730857ece455b6ab2a1166f223705f24d
zmoham2 <zmoham2@uic.edu>
2019-10-15 18:59:08 -0500

    edits to scaling

--- OfficeSpace/Assets/ShrinkSubject.cs
+++ OfficeSpace/Assets/ShrinkSubject.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class ShrinkSubject : MonoBehaviour
 {
     public GameObject ToSize;
-    public GameObject Shrink, Enlarge, Normalize, Ceiling;
+    public GameObject Shrink, Normalize, PositionTest;
 
-    //int counter = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,66 +18,29 @@ public class ShrinkSubject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       /* if (Input.GetKeyDown(KeyCode.T))
-        {
-            ScaleDown();
-        }
-
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            ScaleNormal();
-        }
-
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            ScaleBig();
-        }
-        if (Input.GetKey(KeyCode.JoystickButton9))
-        {
-            ScaleNormal();
-        }
-        if (Input.GetKey(KeyCode.JoystickButton17))
-        {
-            ScaleDown();
-        }*/
+       
 
     }
 
     public void ScaleDown()
     {
-
-        /*if (Enlarge.activeSelf == false)
-        {
+        
             //get the Input from Horizontal axis
             float horizontalInput = Input.GetAxis("Horizontal");
             //get the Input from Vertical axis
             float verticalInput = Input.GetAxis("Vertical");
-            //update the position
-            ToSize.transform.localScale -= new Vector3(4.4f, 4.4f, 4.4f);
-            // Widen the object by x, y, and z values
-            ToSize.transform.position = ToSize.transform.position + new Vector3(horizontalInput, verticalInput, 0);
-            Debug.Log("We came from big state");
 
-        }
-        else if (Enlarge.activeSelf == true)
-        {*/
-            //get the Input from Horizontal axis
-            float horizontalInput = Input.GetAxis("Horizontal");
-            //get the Input from Vertical axis
-            float verticalInput = Input.GetAxis("Vertical");
+            ToSize.transform.position = PositionTest.transform.position;
             //update the position
             ToSize.transform.localScale -= new Vector3(0.7f, 0.7f, 0.7f);
             // Widen the object by x, y, and z values
-            ToSize.transform.position = ToSize.transform.position + new Vector3(horizontalInput, verticalInput, 0);
+            //ToSize.transform.position = ToSize.transform.position + new Vector3(horizontalInput, verticalInput, 0);
             //Debug.Log("We came from normal state");
         //}
 
         Shrink.SetActive(false);
         Normalize.SetActive(true);
-        Enlarge.SetActive(true);
-        Ceiling.SetActive(true);
-
-
+   
     }
     /*public void ScaleBig()
     {
@@ -124,15 +86,25 @@ public class ShrinkSubject : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         //get the Input from Vertical axis
         float verticalInput = Input.GetAxis("Vertical");
+        Debug.Log("Old: " + horizontalInput);
+        Debug.Log("Old: " + verticalInput);
 
         //update the position
+       
+        ToSize.transform.position = ToSize.transform.position - new Vector3(horizontalInput, verticalInput, 0);
         ToSize.transform.localScale = new Vector3(1f, 1f, 1f);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Debug.Log("New: " + horizontalInput);
+        Debug.Log("New: " + verticalInput);
+
+        //ToSize.transform.position = PositionTest.transform.position;
         // Widen the object by x, y, and z values
-        ToSize.transform.position = ToSize.transform.position + new Vector3(horizontalInput, verticalInput, 0);
+        //ToSize.transform.position = ToSize.transform.position + new Vector3(horizontalInput, verticalInput, 0);
         Shrink.SetActive(true);
         Normalize.SetActive(false);
-        Enlarge.SetActive(true);
-        Ceiling.SetActive(true);
 
     }
 }
