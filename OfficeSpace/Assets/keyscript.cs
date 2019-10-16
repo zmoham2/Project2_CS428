a1fe3c3888f1f2522f08a90e262d6ac22a3f5312
MDYbarra <mybarr3@uic.edu>
2019-10-16 14:26:44 -0500

    working on physics Save point

---
+++ OfficeSpace/Assets/keyscript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 30 * (Time.deltaTime), 0  ); //rotates 45 degrees per second around z axis
+
+    }
+}
