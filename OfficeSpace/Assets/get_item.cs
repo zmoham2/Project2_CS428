5019b83cea14d0ca9027bc3720322ebda3686521
MDYbarra <mybarr3@uic.edu>
2019-10-16 21:10:27 -0500

    key,ant complete

---
+++ OfficeSpace/Assets/get_item.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class get_item : MonoBehaviour
+{
+    public GameObject key;
+    public AudioSource getRubyFX;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getItem()
+    {
+        if (!key.activeInHierarchy)
+        {
+            getRubyFX.Play();
+        }
+
+
+        if (key.activeInHierarchy)
+        {
+
+        }
+    }
+
+
+}
